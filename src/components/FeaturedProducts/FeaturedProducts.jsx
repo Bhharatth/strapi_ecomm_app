2a4263fcc97003c236c7de0ff63c8fd114c4c79e
bharath <bharathprasannan96@gmail.com>
2023-01-14 07:31:05 +0530

    strapi connection (fetching items)

--- src/components/FeaturedProducts/FeaturedProducts.jsx
+++ src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,63 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "../Card/Card";
 import "./FeaturedProducts.scss";
+import axios from "axios";
 
-const FeaturedProducts = ({type}) => {
+const FeaturedProducts = ({ type }) => {
+  
 
-    const data = [
-        {id:1,
-        img: "https://images.pexels.com/photos/1656684/pexels-photo-1656684.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        img2:"https://images.pexels.com/photos/991509/pexels-photo-991509.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        isNew:true,
-        oldPrice: 19,
-        price: 12,
-    },
-    {
-        id:2,
-        img: "https://images.pexels.com/photos/1484807/pexels-photo-1484807.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        title: "coat",
-        isNew: true,
-        oldPrice: 19,
-        price:12
+  const [data, setData] = useState([]);
 
-    },
-    {
-        id:3,
-        img: "https://images.pexels.com/photos/1484807/pexels-photo-1484807.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        title: "skirt",
-        isNew: true,
-        oldPrice: 19,
-        price:12
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(
+          process.env.REACT_APP_API_URL + "/products?populate=*",
+          {
+            headers: {
+              Authorization: "bearer " + process.env.REACT_APP_API_TOKEN,
+            },
+          }
+        );
+        setData(res.data.data)
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, []);
+  console.log(data)
 
-    },
-    {
-        id:4,
-        img: "https://images.pexels.com/photos/1484807/pexels-photo-1484807.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        title: "shirt",
-        isNew: true,
-        oldPrice: 19,
-        price:12
-
-    },
-    {
-        id:5,
-        img: "https://images.pexels.com/photos/1484807/pexels-photo-1484807.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        title: "pants",
-        isNew: true,
-        oldPrice: 19,
-        price:12
-
-    },
-    {
-        id:6,
-        img: "https://images.pexels.com/photos/1484807/pexels-photo-1484807.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        title: "caps",
-        isNew: true,
-        oldPrice: 19,
-        price:12
-
-    },
-    ]
   return (
     <div className="featuredProducts">
       <div className="top">
@@ -70,10 +40,9 @@ const FeaturedProducts = ({type}) => {
         </p>
       </div>
       <div className="bottom">
-        {data.map((item)=>(
-            <Card item={item} key={item.id}/>
+        {data.map((item) => (
+          <Card item={item} key={item.id} />
         ))}
-        
       </div>
     </div>
   );
